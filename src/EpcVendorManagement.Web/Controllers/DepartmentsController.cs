58dc7003ebc0ebe7ae9f10f02db9bcb1cdca839c
Saurabhzambare <43110882+Saurabhzambare@users.noreply.github.com>
2026-07-16 13:10:51 +0530

    feat: add DepartmentsController with Index view and navigation

---
+++ src/EpcVendorManagement.Web/Controllers/DepartmentsController.cs
@@ -0,0 +1,24 @@
+﻿using EpcVendorManagement.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EpcVendorManagement.Web.Controllers;
+
+public class DepartmentsController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public DepartmentsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var departments = await _context.Departments
+            .OrderBy(department => department.Name)
+            .ToListAsync();
+
+        return View(departments);
+    }
+}
---
+++ src/EpcVendorManagement.Web/Views/Departments/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EpcVendorManagement.Web.Views.Departments
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
