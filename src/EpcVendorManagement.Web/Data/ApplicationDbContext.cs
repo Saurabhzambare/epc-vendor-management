fb52099683df4a7effd00c62a163bbd82f3838aa
Saurabhzambare <43110882+Saurabhzambare@users.noreply.github.com>
2026-07-16 12:09:37 +0530

    feat: add Department entity, ApplicationDbContext, and initial migration

---
+++ src/EpcVendorManagement.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using EpcVendorManagement.Web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EpcVendorManagement.Web.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Department> Departments =>
+        Set<Department>();
+
+    protected override void OnModelCreating(
+        ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Department>()
+            .HasIndex(department => department.Name)
+            .IsUnique();
+    }
+}
---
+++ src/EpcVendorManagement.Web/Models/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EpcVendorManagement.Web.Models.Entities;
+
+public class Department
+{
+    public int Id { get; set; }
+
+    [Required, StringLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [StringLength(500)]
+    public string? Description { get; set; }
+
+    public bool IsActive { get; set; } = true;
+}
